a8240fef9efc4c3da185fb3bc9771be05c043719
Artur <artur3087@gmail.com>
2024-01-28 17:14:35 +0000

    2nd commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,19 @@ export default function Home () { // START of default function //
 
   return ( // return START //
   
-    
+    <div>
+      <h1 className="text-[5rem]"> DUDES' WEBSITE</h1>
+      <nav className="flex gap-10">
+        <a className="text-[3rem]" href="#">NOWHERE but bigger ! xD</a>
+        <a href="#">NOWHERE</a>
+        <a href="#">NOWHERE</a>
+        <a href="#">NOWHERE</a>
+      </nav>
+      <h2 className="text-[2rem]">
+        HOME PAGE, DUDE! 
+      </h2>
+      <p>Hi there, dude!</p>
+    </div>
   
   ) // return END //
 
